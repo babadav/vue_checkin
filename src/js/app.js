dba1cc5eec3ab0be21b9eee02488bc85b4e3e301
babadav <babadav@gmail.com>
2018-01-17 13:42:00 -0500

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var app = new Vue({
+	el: "#app",
+	data: {
+		enteredName: '',
+		enteredEmail: '',
+		people: [
+			{
+				name: 'John Johnson',
+				email: 'johnson@johnsonandjohnson.com',
+			},
+			{
+				name: 'Fred Fredrickson',
+				email: 'fredrison@gmail.com',
+			},
+			{
+				name: 'Brad Pitt',
+				email: 'brad@pitt.com',
+			},
+		]
+	},
+})
+
+
+
+
+
