9f4036b9aa6ebafdf44ab72b11061bdaa66ab5d6
babadav <babadav@gmail.com>
2018-01-17 15:25:10 -0500

    after online class

--- src/js/app.js
+++ src/js/app.js
@@ -9,19 +9,57 @@ var app = new Vue({
 			{
 				name: 'John Johnson',
 				email: 'johnson@johnsonandjohnson.com',
+				time: Date.now(),
+				since: 0, // number of minutes since person has checked in.
 			},
 			{
 				name: 'Fred Fredrickson',
 				email: 'fredrison@gmail.com',
+				time: Date.now(),
+				since: 0,
 			},
 			{
 				name: 'Brad Pitt',
 				email: 'brad@pitt.com',
+				time: Date.now(),
+				since: 0,
 			},
 		]
 	},
-})
+	created: function(){
+		setInterval(this.updateTimeSinceValues, 10 * 1000)
 
+
+	},
+	methods:{
+		updateTimeSinceValues: function(){
+			console.log('10sec')
+			this.people.forEach(person => {
+				person.since = Math.round((Date.now() - person.time) / 1000 /60)
+			})
+		},
+		submitPerson: function() {
+			if (!this.enteredEmail || !this.enteredName) 
+				return
+			console.log('enter was hit')
+
+			this.people.unshift(
+				{
+				name: this.enteredName,
+				email: this.enteredEmail,
+				time: Date.now(),
+			});
+
+			if (this.people.length > 4) {
+				this.people.pop();
+			}
+
+			this.enteredEmail = ''
+			this.enteredName = ''
+		}
+
+	}
+})
 
 
 
